866c61087e6245118909b07a267f4f97a20d841d
CrazyMage528 <ursaev.r@yandex.ru>
2024-09-13 15:01:15 +0500

    A2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,17 +6,17 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
 
-        // Получение и вывод бина HelloWorld
+
         HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
 
         System.out.println(bean1.getMessage());
-        System.out.println(bean1 == bean2); // Должно вывести true, так как Singleton
+        System.out.println(bean1 == bean2);
 
         // Получение и вывод бина Cat
         Cat cat1 = (Cat) applicationContext.getBean("cat");
         Cat cat2 = (Cat) applicationContext.getBean("cat");
 
-        System.out.println(cat1 == cat2); // Должно вывести false, так как Prototype
+        System.out.println(cat1 == cat2);
     }
 }
